4cd651c2b79e0df4fd8a33312cd1cafdb1a289fd
Martin Geisler <martin@geisler.net>
2017-10-29 09:41:26 +0100

    benches: take test function name direction
    We no longer need the extra closures.

--- benches/comparison.rs
+++ benches/comparison.rs
@@ -5,18 +5,17 @@ extern crate rand;
 extern crate smawk;
 extern crate test;
 
-use ndarray::Array2;
 use rand::XorShiftRng;
 use test::Bencher;
 
 macro_rules! repeat {
-    ([ $( ($func:ident, $size:expr) $(,)* )* ], $body:expr) => {
+    ([ $( ($func:ident, $size:expr) $(,)* )* ], $row_func:path) => {
         $(
             #[bench]
             fn $func(b: &mut Bencher) {
                 let mut rng = XorShiftRng::new_unseeded();
                 let matrix = smawk::random_monge_matrix($size, $size, &mut rng);
-                b.iter(|| $body(&matrix));
+                b.iter(|| $row_func(&matrix));
             }
         )*
     };
@@ -27,7 +26,7 @@ repeat!([(brute_force_025, 25),
          (brute_force_100, 100),
          (brute_force_200, 200),
          (brute_force_400, 400)],
-        |matrix: &Array2<i32>| smawk::brute_force_row_minima(&matrix));
+        smawk::brute_force_row_minima);
 
 repeat!([(recursive_0025, 25),
          (recursive_0050, 50),
@@ -35,7 +34,7 @@ repeat!([(recursive_0025, 25),
          (recursive_0200, 200),
          (recursive_0400, 400),
          (recursive_0800, 800)],
-        |matrix: &Array2<i32>| smawk::recursive_row_minima(&matrix));
+        smawk::recursive_row_minima);
 
 repeat!([(smawk_025, 25),
          (smawk_050, 50),
@@ -43,4 +42,4 @@ repeat!([(smawk_025, 25),
          (smawk_200, 200),
          (smawk_400, 400),
          (smawk_800, 800)],
-        |matrix: &Array2<i32>| smawk::smawk_row_minima(&matrix));
+        smawk::smawk_row_minima);
