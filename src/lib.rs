fad67558d7f1dcfe4663e7e443c59f84e2483e3b
Martin Geisler <martin@geisler.net>
2020-08-17 11:14:05 +0200

    Move brute-force and recursive algorithms to separate modules
    These functions are mainly there for testing purposes so the code
    should not sit around next to the main code.
    
    This is a step towards the goal of not depending on ndarray.

--- src/lib.rs
+++ src/lib.rs
@@ -91,109 +91,11 @@
 
 #![doc(html_root_url = "https://docs.rs/smawk/0.2.0")]
 
-use ndarray::{s, Array2, ArrayView1, ArrayView2, Axis};
-use num_traits::{PrimInt, WrappingAdd};
-use rand::distributions::{Distribution, Standard};
-use rand::Rng;
+use ndarray::Array2;
 
-/// Compute lane minimum by brute force.
-///
-/// This does a simple scan through the lane (row or column).
-#[inline]
-fn lane_minimum<T: Ord>(lane: ArrayView1<'_, T>) -> usize {
-    lane.iter()
-        .enumerate()
-        .min_by_key(|&(idx, elem)| (elem, idx))
-        .map(|(idx, _)| idx)
-        .expect("empty lane in matrix")
-}
-
-/// Compute row minima by brute force in O(*mn*) time.
-///
-/// # Panics
-///
-/// It is an error to call this on a matrix with zero columns.
-pub fn brute_force_row_minima<T: Ord>(matrix: &Array2<T>) -> Vec<usize> {
-    matrix.genrows().into_iter().map(lane_minimum).collect()
-}
-
-/// Compute column minima by brute force in O(*mn*) time.
-///
-/// # Panics
-///
-/// It is an error to call this on a matrix with zero rows.
-pub fn brute_force_column_minima<T: Ord>(matrix: &Array2<T>) -> Vec<usize> {
-    matrix.gencolumns().into_iter().map(lane_minimum).collect()
-}
-
-/// Compute row minima in O(*m* + *n* log *m*) time.
-///
-/// # Panics
-///
-/// It is an error to call this on a matrix with zero columns.
-pub fn recursive_row_minima<T: Ord>(matrix: &Array2<T>) -> Vec<usize> {
-    let mut minima = vec![0; matrix.nrows()];
-    recursive_inner(matrix.view(), &|| Direction::Row, 0, &mut minima);
-    minima
-}
-
-/// Compute column minima in O(*n* + *m* log *n*) time.
-///
-/// # Panics
-///
-/// It is an error to call this on a matrix with zero rows.
-pub fn recursive_column_minima<T: Ord>(matrix: &Array2<T>) -> Vec<usize> {
-    let mut minima = vec![0; matrix.ncols()];
-    recursive_inner(matrix.view(), &|| Direction::Column, 0, &mut minima);
-    minima
-}
-
-/// The type of minima (row or column) we compute.
-enum Direction {
-    Row,
-    Column,
-}
-
-/// Compute the minima along the given direction (`Direction::Row` for
-/// row minima and `Direction::Column` for column minima).
-///
-/// The direction is given as a generic function argument to allow
-/// monomorphization to kick in. The function calls will be inlined
-/// and optimized away and the result is that the compiler generates
-/// differnet code for finding row and column minima.
-fn recursive_inner<T: Ord, F: Fn() -> Direction>(
-    matrix: ArrayView2<'_, T>,
-    dir: &F,
-    offset: usize,
-    minima: &mut [usize],
-) {
-    if matrix.is_empty() {
-        return;
-    }
-
-    let axis = match dir() {
-        Direction::Row => Axis(0),
-        Direction::Column => Axis(1),
-    };
-    let mid = matrix.len_of(axis) / 2;
-    let min_idx = lane_minimum(matrix.index_axis(axis, mid));
-    minima[mid] = offset + min_idx;
-
-    if mid == 0 {
-        return; // Matrix has a single row or column, so we're done.
-    }
-
-    let top_left = match dir() {
-        Direction::Row => matrix.slice(s![..mid, ..(min_idx + 1)]),
-        Direction::Column => matrix.slice(s![..(min_idx + 1), ..mid]),
-    };
-    let bot_right = match dir() {
-        Direction::Row => matrix.slice(s![(mid + 1).., min_idx..]),
-        Direction::Column => matrix.slice(s![min_idx.., (mid + 1)..]),
-    };
-    recursive_inner(top_left, dir, offset, &mut minima[..mid]);
-    recursive_inner(bot_right, dir, offset + min_idx, &mut minima[mid + 1..]);
-}
+pub mod brute_force;
+pub mod monge;
+pub mod recursive;
 
 /// Compute row minima in O(*m* + *n*) time.
 ///
@@ -424,303 +326,12 @@ pub fn online_column_minima<T: Copy + Ord, M: Fn(&[(usize, T)], usize, usize) ->
     result
 }
 
-/// Verify that a matrix is a Monge matrix.
-///
-/// A [Monge matrix] \(or array) is a matrix where the following
-/// inequality holds:
-///
-/// ```text
-/// M[i, j] + M[i', j'] <= M[i, j'] + M[i', j]  for all i < i', j < j'
-/// ```
-///
-/// The inequality says that the sum of the main diagonal is less than
-/// the sum of the antidiagonal. Checking this condition is done by
-/// checking *n* ✕ *m* submatrices, so the running time is O(*mn*).
-///
-/// [Monge matrix]: https://en.wikipedia.org/wiki/Monge_array
-pub fn is_monge<T: PrimInt + WrappingAdd>(matrix: &Array2<T>) -> bool {
-    matrix.windows([2, 2]).into_iter().all(|sub| {
-        let (x, y) = (sub[[0, 0]], sub[[1, 1]]);
-        let (z, w) = (sub[[0, 1]], sub[[1, 0]]);
-        match (x.checked_add(&y), z.checked_add(&w)) {
-            (Some(a), Some(b)) => a <= b,
-            (Some(_), None) => true,
-            (None, Some(_)) => false,
-            (None, None) => x.wrapping_add(&y) <= z.wrapping_add(&w),
-        }
-    })
-}
-
-/// A Monge matrix can be decomposed into one of these primitive
-/// building blocks.
-#[derive(Copy, Clone)]
-enum MongePrim {
-    ConstantRows,
-    ConstantCols,
-    UpperRightOnes,
-    LowerLeftOnes,
-}
-
-impl MongePrim {
-    /// Generate a Monge matrix from a primitive.
-    fn to_matrix<T: PrimInt, R: Rng>(&self, m: usize, n: usize, rng: &mut R) -> Array2<T>
-    where
-        Standard: Distribution<T>,
-    {
-        let mut matrix = Array2::from_elem((m, n), T::zero());
-        // Avoid panic in UpperRightOnes and LowerLeftOnes below.
-        if m == 0 || n == 0 {
-            return matrix;
-        }
-
-        match *self {
-            MongePrim::ConstantRows => {
-                for mut row in matrix.genrows_mut() {
-                    if rng.gen::<bool>() {
-                        row.fill(T::one())
-                    }
-                }
-            }
-            MongePrim::ConstantCols => {
-                for mut col in matrix.gencolumns_mut() {
-                    if rng.gen::<bool>() {
-                        col.fill(T::one())
-                    }
-                }
-            }
-            MongePrim::UpperRightOnes => {
-                let i = rng.gen_range(0, (m + 1) as isize);
-                let j = rng.gen_range(0, (n + 1) as isize);
-                matrix.slice_mut(s![..i, -j..]).fill(T::one());
-            }
-            MongePrim::LowerLeftOnes => {
-                let i = rng.gen_range(0, (m + 1) as isize);
-                let j = rng.gen_range(0, (n + 1) as isize);
-                matrix.slice_mut(s![-i.., ..j]).fill(T::one());
-            }
-        }
-
-        matrix
-    }
-}
-
-/// Generate a random Monge matrix.
-pub fn random_monge_matrix<R: Rng, T: PrimInt>(m: usize, n: usize, rng: &mut R) -> Array2<T>
-where
-    Standard: Distribution<T>,
-{
-    let monge_primitives = [
-        MongePrim::ConstantRows,
-        MongePrim::ConstantCols,
-        MongePrim::LowerLeftOnes,
-        MongePrim::UpperRightOnes,
-    ];
-    let mut matrix = Array2::from_elem((m, n), T::zero());
-    for _ in 0..(m + n) {
-        let monge = monge_primitives[rng.gen_range(0, monge_primitives.len())];
-        matrix = matrix + monge.to_matrix(m, n, rng);
-    }
-    matrix
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
-    use ndarray::{arr2, Array};
+    use ndarray::{arr2, s};
     use rand::SeedableRng;
     use rand_chacha::ChaCha20Rng;
-
-    #[test]
-    fn is_monge_handles_overflow() {
-        // The x + y <= z + w computations will overflow for an u8
-        // matrix unless is_monge is careful.
-        let matrix: Array2<u8> = arr2(&[
-            [200, 200, 200, 200],
-            [200, 200, 200, 200],
-            [200, 200, 200, 200],
-        ]);
-        assert!(is_monge(&matrix));
-    }
-
-    #[test]
-    fn monge_constant_rows() {
-        let mut rng = ChaCha20Rng::seed_from_u64(0);
-        let matrix: Array2<u8> = MongePrim::ConstantRows.to_matrix(5, 4, &mut rng);
-        assert!(is_monge(&matrix));
-        for row in matrix.genrows() {
-            let elem = row[0];
-            assert_eq!(row, Array::from_elem(matrix.ncols(), elem));
-        }
-    }
-
-    #[test]
-    fn monge_constant_cols() {
-        let mut rng = ChaCha20Rng::seed_from_u64(0);
-        let matrix: Array2<u8> = MongePrim::ConstantCols.to_matrix(5, 4, &mut rng);
-        assert!(is_monge(&matrix));
-        for column in matrix.gencolumns() {
-            let elem = column[0];
-            assert_eq!(column, Array::from_elem(matrix.nrows(), elem));
-        }
-    }
-
-    #[test]
-    fn monge_upper_right_ones() {
-        let mut rng = ChaCha20Rng::seed_from_u64(1);
-        let matrix: Array2<u8> = MongePrim::UpperRightOnes.to_matrix(5, 4, &mut rng);
-        assert!(is_monge(&matrix));
-        assert_eq!(
-            matrix,
-            arr2(&[
-                [0, 0, 1, 1],
-                [0, 0, 1, 1],
-                [0, 0, 1, 1],
-                [0, 0, 0, 0],
-                [0, 0, 0, 0]
-            ])
-        );
-    }
-
-    #[test]
-    fn monge_lower_left_ones() {
-        let mut rng = ChaCha20Rng::seed_from_u64(1);
-        let matrix: Array2<u8> = MongePrim::LowerLeftOnes.to_matrix(5, 4, &mut rng);
-        assert!(is_monge(&matrix));
-        assert_eq!(
-            matrix,
-            arr2(&[
-                [0, 0, 0, 0],
-                [0, 0, 0, 0],
-                [1, 1, 0, 0],
-                [1, 1, 0, 0],
-                [1, 1, 0, 0]
-            ])
-        );
-    }
-
-    #[test]
-    fn brute_force_1x1() {
-        let matrix = arr2(&[[2]]);
-        let minima = vec![0];
-        assert_eq!(brute_force_row_minima(&matrix), minima);
-        assert_eq!(brute_force_column_minima(&matrix.reversed_axes()), minima);
-    }
-
-    #[test]
-    fn brute_force_2x1() {
-        let matrix = arr2(&[[3], [2]]);
-        let minima = vec![0, 0];
-        assert_eq!(brute_force_row_minima(&matrix), minima);
-        assert_eq!(brute_force_column_minima(&matrix.reversed_axes()), minima);
-    }
-
-    #[test]
-    fn brute_force_1x2() {
-        let matrix = arr2(&[[2, 1]]);
-        let minima = vec![1];
-        assert_eq!(brute_force_row_minima(&matrix), minima);
-        assert_eq!(brute_force_column_minima(&matrix.reversed_axes()), minima);
-    }
-
-    #[test]
-    fn brute_force_2x2() {
-        let matrix = arr2(&[[3, 2], [2, 1]]);
-        let minima = vec![1, 1];
-        assert_eq!(brute_force_row_minima(&matrix), minima);
-        assert_eq!(brute_force_column_minima(&matrix.reversed_axes()), minima);
-    }
-
-    #[test]
-    fn brute_force_3x3() {
-        let matrix = arr2(&[[3, 4, 4], [3, 4, 4], [2, 3, 3]]);
-        let minima = vec![0, 0, 0];
-        assert_eq!(brute_force_row_minima(&matrix), minima);
-        assert_eq!(brute_force_column_minima(&matrix.reversed_axes()), minima);
-    }
-
-    #[test]
-    fn brute_force_4x4() {
-        let matrix = arr2(&[[4, 5, 5, 5], [2, 3, 3, 3], [2, 3, 3, 3], [2, 2, 2, 2]]);
-        let minima = vec![0, 0, 0, 0];
-        assert_eq!(brute_force_row_minima(&matrix), minima);
-        assert_eq!(brute_force_column_minima(&matrix.reversed_axes()), minima);
-    }
-
-    #[test]
-    fn brute_force_5x5() {
-        let matrix = arr2(&[
-            [3, 2, 4, 5, 6],
-            [2, 1, 3, 3, 4],
-            [2, 1, 3, 3, 4],
-            [3, 2, 4, 3, 4],
-            [4, 3, 2, 1, 1],
-        ]);
-        let minima = vec![1, 1, 1, 1, 3];
-        assert_eq!(brute_force_row_minima(&matrix), minima);
-        assert_eq!(brute_force_column_minima(&matrix.reversed_axes()), minima);
-    }
-
-    #[test]
-    fn recursive_1x1() {
-        let matrix = arr2(&[[2]]);
-        let minima = vec![0];
-        assert_eq!(recursive_row_minima(&matrix), minima);
-        assert_eq!(recursive_column_minima(&matrix.reversed_axes()), minima);
-    }
-
-    #[test]
-    fn recursive_2x1() {
-        let matrix = arr2(&[[3], [2]]);
-        let minima = vec![0, 0];
-        assert_eq!(recursive_row_minima(&matrix), minima);
-        assert_eq!(recursive_column_minima(&matrix.reversed_axes()), minima);
-    }
-
-    #[test]
-    fn recursive_1x2() {
-        let matrix = arr2(&[[2, 1]]);
-        let minima = vec![1];
-        assert_eq!(recursive_row_minima(&matrix), minima);
-        assert_eq!(recursive_column_minima(&matrix.reversed_axes()), minima);
-    }
-
-    #[test]
-    fn recursive_2x2() {
-        let matrix = arr2(&[[3, 2], [2, 1]]);
-        let minima = vec![1, 1];
-        assert_eq!(recursive_row_minima(&matrix), minima);
-        assert_eq!(recursive_column_minima(&matrix.reversed_axes()), minima);
-    }
-
-    #[test]
-    fn recursive_3x3() {
-        let matrix = arr2(&[[3, 4, 4], [3, 4, 4], [2, 3, 3]]);
-        let minima = vec![0, 0, 0];
-        assert_eq!(recursive_row_minima(&matrix), minima);
-        assert_eq!(recursive_column_minima(&matrix.reversed_axes()), minima);
-    }
-
-    #[test]
-    fn recursive_4x4() {
-        let matrix = arr2(&[[4, 5, 5, 5], [2, 3, 3, 3], [2, 3, 3, 3], [2, 2, 2, 2]]);
-        let minima = vec![0, 0, 0, 0];
-        assert_eq!(recursive_row_minima(&matrix), minima);
-        assert_eq!(recursive_column_minima(&matrix.reversed_axes()), minima);
-    }
-
-    #[test]
-    fn recursive_5x5() {
-        let matrix = arr2(&[
-            [3, 2, 4, 5, 6],
-            [2, 1, 3, 3, 4],
-            [2, 1, 3, 3, 4],
-            [3, 2, 4, 3, 4],
-            [4, 3, 2, 1, 1],
-        ]);
-        let minima = vec![1, 1, 1, 1, 3];
-        assert_eq!(recursive_row_minima(&matrix), minima);
-        assert_eq!(recursive_column_minima(&matrix.reversed_axes()), minima);
-    }
 
     #[test]
     fn smawk_1x1() {
@@ -794,11 +405,11 @@ mod tests {
         for _ in 0..4 {
             for m in sizes.clone().iter() {
                 for n in sizes.clone().iter() {
-                    let matrix: Array2<i32> = random_monge_matrix(*m, *n, &mut rng);
+                    let matrix: Array2<i32> = monge::random_monge_matrix(*m, *n, &mut rng);
 
                     // Compute and test row minima.
-                    let brute_force = brute_force_row_minima(&matrix);
-                    let recursive = recursive_row_minima(&matrix);
+                    let brute_force = brute_force::row_minima(&matrix);
+                    let recursive = recursive::row_minima(&matrix);
                     let smawk = smawk_row_minima(&matrix);
                     assert_eq!(
                         brute_force, recursive,
@@ -812,8 +423,8 @@ mod tests {
                     );
 
                     // Do the same for the column minima.
-                    let brute_force = brute_force_column_minima(&matrix);
-                    let recursive = recursive_column_minima(&matrix);
+                    let brute_force = brute_force::column_minima(&matrix);
+                    let recursive = recursive::column_minima(&matrix);
                     let smawk = smawk_column_minima(&matrix);
                     assert_eq!(
                         brute_force, recursive,
@@ -894,10 +505,10 @@ mod tests {
             for &size in &sizes {
                 // Random totally monotone square matrix of the
                 // desired size.
-                let mut matrix: Array2<i32> = random_monge_matrix(size, size, &mut rng);
+                let mut matrix: Array2<i32> = monge::random_monge_matrix(size, size, &mut rng);
 
                 // Adjust matrix so the column minima are above the
-                // diagonal. The brute_force_column_minima will still
+                // diagonal. The brute_force::column_minima will still
                 // work just fine on such a mangled Monge matrix.
                 let max = *matrix.iter().max().unwrap_or(&0);
                 for idx in 0..(size as isize) {
@@ -917,7 +528,7 @@ mod tests {
 
                 // Brute-force computation of column minima, returned
                 // in the same form as online_column_minima.
-                let brute_force = brute_force_column_minima(&matrix)
+                let brute_force = brute_force::column_minima(&matrix)
                     .iter()
                     .enumerate()
                     .map(|(j, &i)| (i, matrix[[i, j]]))
