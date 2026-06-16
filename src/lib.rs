e0389af917bf10f4075a57e510b5fec520154a4a
Martin Geisler <martin@geisler.net>
2026-06-16 07:42:47 +0000

    Bump version to 0.3.3

--- src/lib.rs
+++ src/lib.rs
@@ -131,7 +131,7 @@
 //!   *Software: Practice and Experience*, 11(11):1119–1184, 1981.
 
 #![no_std]
-#![doc(html_root_url = "https://docs.rs/smawk/0.3.2")]
+#![doc(html_root_url = "https://docs.rs/smawk/0.3.3")]
 // The s! macro from ndarray uses unsafe internally, so we can only
 // forbid unsafe code when building with the default features.
 #![cfg_attr(not(feature = "ndarray"), forbid(unsafe_code))]
